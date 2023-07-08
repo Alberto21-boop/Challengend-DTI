a80da1c3ed3ee4a7f1bb1863fcee8dee9f50a59e
Alberto <albertobarbosa0003@gmail.com>
2023-07-08 13:34:17 -0300

    We're almost there

--- src/components/pages/Expense/index.tsx
+++ src/components/pages/Expense/index.tsx
@@ -2,20 +2,36 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../../Header";
 
-type RepositorySenatorsExpenses = {
-  id: number;
-  type: number;
-  nomeSenador: string;
-  supplier: string; // fornecedor
-  year: number; // ano
-  month: number; // mês
-  day: number; // dia
-  valor: number; // valor 
+type ExpenseType = {
+  tipo: number;
+  fornec: string;
+  ano: number;
+  mes: number;
+  dia: number;
+  valor: number;
 };
+
+type SenatorExpense = {
+  id: number;
+  nomeSenador: string;
+  despesas: ExpenseType[];
+};
+
+type RepositorySenatorsExpenses = SenatorExpense[];
+
+const typeLabels: string[] = [
+  "Aluguel de imóveis e despesas concernentes a eles",
+  "Divulgação da atividade parlamentar",
+  "Aquisição de material de consumo para uso no escritório",
+  "Passagens aéreas, aquáticas e terrestres nacionais",
+  "Contratação de consultorias, assessorias, pesquisas, trabalhos técnicos e outros serviços",
+  "Locomoção, hospedagem, alimentação e combustíveis",
+  "Serviços de Segurança Privada"
+];
 
 export function Expense() {
   const { id } = useParams<{ id: string }>();
-  const [repositorySenatorsExpenses, setRepositorySenatorsExpenses] = useState<RepositorySenatorsExpenses[]>([]);
+  const [repositorySenatorsExpenses, setRepositorySenatorsExpenses] = useState<RepositorySenatorsExpenses>([]);
   const [repositoryPolitician, setRepositoryPolitician] = useState("");
 
   useEffect(() => {
@@ -25,10 +41,10 @@ export function Expense() {
         const politicianData = await responsePolitician.json();
         setRepositoryPolitician(politicianData.nomeSenador);
 
-        const responseExpenses = await fetch(`http://localhost:3000/despesasSenadores?id=${id}`);
+        const responseExpenses = await fetch(`http://localhost:3000/despesasSenadores`);
         const expensesData = await responseExpenses.json();
         setRepositorySenatorsExpenses(expensesData);
-        console.log("expensesData:", expensesData);
+
       } catch (error) {
         console.error(error);
       }
@@ -37,42 +53,27 @@ export function Expense() {
     fetchData();
   }, [id]);
 
-  const calculateTotalByType = (expenses: RepositorySenatorsExpenses[], type: number) => {
-    const filteredExpenses = expenses.filter((expense) => expense.type === type);
+  const calculateTotalByType = (expenses: ExpenseType[], type: number) => {
+    const filteredExpenses = expenses.filter((expense) => expense.tipo === type);
     const total = filteredExpenses.reduce((acc, expense) => {
-      const valor = expense.valor || 0; // Verificação para evitar undefined
-      return acc + Number(valor);
+      return acc + expense.valor;
     }, 0);
     return parseFloat(total.toFixed(2));
   };
 
-
-  const typeLabels = [
-    "Aluguel de imóveis e despesas concernentes a eles",
-    "Divulgação da atividade parlamentar",
-    "Aquisição de material de consumo para uso no escritório",
-    "Passagens aéreas, aquáticas e terrestres nacionais",
-    "Contratação de consultorias, assessorias, pesquisas, trabalhos técnicos e outros serviços",
-    "Locomoção, hospedagem, alimentação e combustíveis",
-    "Serviços de Segurança Privada"
-  ];
-
   const totalByType: number[] = [];
   for (let i = 0; i < 7; i++) {
-    const total = calculateTotalByType(repositorySenatorsExpenses, i + 1);
+    const total = calculateTotalByType(repositorySenatorsExpenses[0]?.despesas || [], i + 1);
     totalByType.push(total);
     console.log(`Total type ${i}:`, total);
   }
 
-  console.log("repositorySenatorsExpenses:", repositorySenatorsExpenses);
-
-
-  const totalGeral = repositorySenatorsExpenses.reduce((total, expense) => {
-    const valor = typeof expense.valor === 'number' ? expense.valor : 0; // Verificação adicional
-    console.log("Total parcial:", total, "+", valor);
-    return total + parseFloat(valor.toFixed(2));
+  const totalGeral = repositorySenatorsExpenses.reduce((total, senator) => {
+    const senatorTotal = senator.despesas.reduce((acc, expense) => {
+      return acc + expense.valor;
+    }, 0);
+    return total + senatorTotal;
   }, 0);
-  console.log("Total geral:", totalGeral);
 
   return (
     <>
@@ -83,13 +84,12 @@ export function Expense() {
       <ul>
         {typeLabels.map((label, index) => (
           <li key={index}>
-            {label}: R$ {totalByType[index].toFixed(2)}
+            {label}: R$ {totalByType[index]?.toFixed(2) || "0.00"}
           </li>
         ))}
       </ul>
 
-      <p>Total Geral: R$ {parseFloat(totalGeral.toFixed(2)).toString()}</p>
-
+      <p>Total Geral: R$ {totalGeral.toFixed(2)}</p>
     </>
   );
 }
